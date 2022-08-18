f5e74fc6f3c321ec9f97b1ad9112e410abac7237
Yathindra <yathindrarawya123@gmail.com>
2022-08-18 16:23:25 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ import { AuthProvider, Storage, useAuthContext } from "@asgardeo/auth-react";
+ import React, { FunctionComponent, ReactElement } from "react";
+ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+ import { ErrorBoundary } from "./error-boundary";
+ import { HomePage, NotFoundPage } from "./pages";
+ import { LandingPage } from "./pages/landing";
+ import { LoggedOutPage } from "./pages/LoggedOut";
+ import "./app.css";
+//  import { ReactNotifications } from "react-notifications-component";
+ 
+ const AppContent: FunctionComponent = (): ReactElement => {
+     const { error } = useAuthContext();
+     
+     return (
+         <>
+             {/* <ReactNotifications /> */}
+             <ErrorBoundary error={error}>
+                 <Router>
+                     <Routes>
+                         <Route path="/" element={<LandingPage />} />
+                         <Route path="/signin" element={<HomePage />} />
+                         <Route path="/login" element={<LoggedOutPage />} />
+                         <Route path="*" element={<NotFoundPage />} />
+                     </Routes>
+                 </Router>
+             </ErrorBoundary>
+         </>
+     )
+ };
+ 
+ const App = () => (
+  <AuthProvider
+    config={ {
+        signInRedirectURL: "https://localhost:3000/signin",
+        signOutRedirectURL: "https://localhost:3000/login",
+        clientID: "<client_id>",
+        baseUrl: "https://api.asgardeo.io/t/<tenant>",
+        scope: [ "openid","profile" ],
+        storage: "webWorker" as Storage.WebWorker
+    } }
+  >
+         <AppContent />
+     </AuthProvider>
+ );
+ 
+ export default App;
